426b1da50274d3fa79744159e4cc318002742e9f
Dylan-Mumaw <dylanmichaelmumaw@gmail.com>
2020-09-30 20:57:34 -0500

    Add files via upload

---
+++ Typing_Test_Prod/Type_Test_Home.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Typing_Test_Prod
+{
+    public partial class Type_Test_Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
